ae13979c12ee59a9c08cb2c0c73ac0adae8be18e
Clara-newbie <m.clara.massaro@gmail.com>
2025-01-16 12:29:25 +0100

    modifica finale

--- ManipolazioneDom1/Manipolazione1.js
+++ ManipolazioneDom1/Manipolazione1.js
@@ -1,15 +1,10 @@
 const ul = document.querySelector("ul");
 
 const addProduct = () => {
+  const inputValue = document.querySelector("input").value;
   const li = document.createElement("li");
   ul.appendChild(li);
-  const input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
-  li.appendChild(input);
-  // COME INSERISCO IL TESTO SENZA PERDERE LA CHECKBOX ?
+
+  li.innerHTML = `<input type="checkbox" /> ${inputValue}`;
   li.style.listStyle = "none";
 };
-
-console.log(addProduct);
-
-/* non mi è del tutto chiaro come faccia a funzionare senza un return */
